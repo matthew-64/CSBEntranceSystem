0839cfdb90b1b25337d834ef068ee087af208589
Matthew Elliott <melliott21@qub.ac.uk>
2020-12-01 17:58:49 +0000

    Added dummy card scanner

---
+++ CardScanner.py
@@ -0,0 +1,7 @@
+import random
+
+student_numbers = range(1, 11)
+
+
+def scan():
+    return random.choice(student_numbers)
--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
-#from flask import request
+from flask import request
 from flask import render_template
 import cv2
+import CardScanner
 #import stringComparison
 
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    #return 'test'
-    return render_template("test.html") # this should be the name of your html file
+    #scanner = CardScanner()
+    app.logger.info(CardScanner.scan())
+    #app.logger.info("STARTING")
+
+    #text1 = request.form['text1']
+    #text2 = request.form['text2']
+    return render_template("test.html")
 
 #@app.route('/', methods=['POST'])
 #def my_form_post():
@@ -21,11 +27,8 @@ def my_form():
 #    else :
 #        return "<h1>No Plagiarism Detected !</h1>"
 
-#@app.route('/')
-#def hello_world():
-#    return 'Hello, World!'
-
-
+def get_student_num(methods=['POST']):
+    return 0;
 
 if __name__ == '__main__':
     app.run(debug=True)
