9f4ac710f2d911be27eff9f1e92dd8acffc704b1
Matthew Elliott <melliott21@qub.ac.uk>
2020-12-01 13:33:15 +0000

    Added simple flask server

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+#import stringComparison
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    #return 'test'
+    return render_template("home.html") # this should be the name of your html file
+
+#@app.route('/', methods=['POST'])
+#def my_form_post():
+#    text1 = request.form['text1']
+#    text2 = request.form['text2']
+#    plagiarismPercent = stringComparison.extremelySimplePlagiarismChecker(text1,text2)
+#    if plagiarismPercent > 50 :
+#        return "<h1>Plagiarism Detected !</h1>"
+#    else :
+#        return "<h1>No Plagiarism Detected !</h1>"
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
