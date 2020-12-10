a1970bdf9cd1f9fce690c90e5191055a2e19bc9d
Matthew Elliott <melliott21@qub.ac.uk>
2020-12-10 03:09:01 +0000

    Send and revieve data between flask server and html page

--- App.py
+++ App.py
@@ -3,36 +3,37 @@ from flask import request
 from flask import render_template
 import cv2
 import CardScanner
+import random
 #import stringComparison
 
 app = Flask(__name__)
 #https://stackoverflow.com/questions/46482475/how-handle-a-button-click-on-python-flask/46482985
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def my_form():
-    #app.logger.info(CardScanner.scan())
-
-    #if request.method == "POST":
-    #    print("IS a post request")
-
-    return render_template("test.html")
+    student_num = request.form["student_num"]
+    print(student_num)
+    return render_template("test.html", message=student_num)
 
 #background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
-    return ("nothing")
-#@app.route('/', methods=['POST'])
-#def my_form_post():
-#    text1 = request.form['text1']
-#    text2 = request.form['text2']
-#    plagiarismPercent = stringComparison.extremelySimplePlagiarismChecker(text1,text2)
-#    if plagiarismPercent > 50 :
-#        return "<h1>Plagiarism Detected !</h1>"
-#    else :
-#        return "<h1>No Plagiarism Detected !</h1>"
+#@app.route('/background_process_test')
+#def background_process_test():
 
-def get_student_num(methods=['POST']):
-    return 0
+#    print("Checking ")
+#    print ("Hello")
+#    return
+
+# when got, this method is called
+# Preform the calculations here, and then return the result :)
+#@app.route("/data", methods=["GET", "POST"])
+#def data():
+    #data = "THIS IS UPDATED"
+    #response = make_response(json.dumps(data))
+    #response.content_type = "application/json"
+#    return str(random.randrange(0, 10))
+
+#@app.route("/msg")
+#def update_msg():
+#    return render_template("test.html", message="MY MESSAGE")
 
 if __name__ == '__main__':
     app.run(debug=True)
